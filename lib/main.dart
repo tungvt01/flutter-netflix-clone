21cadc60fd345859eb2f3693f0370e71e3f39e8b
Tung Vu <tungvt.it.01@gmail.com>
2025-03-07 17:43:54 +0900

    Config native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Netflix Clone',
       themeMode: ThemeMode.dark,
       theme: ThemeData(
