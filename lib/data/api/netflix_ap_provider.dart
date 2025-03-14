32d0c1395fcec3b76e8b2de818a12dbd080afdb4
Tung Vu <tungvt.it.01@gmail.com>
2025-03-14 19:02:14 +0900

    Improve http request log

--- lib/data/api/netflix_ap_provider.dart
+++ lib/data/api/netflix_ap_provider.dart
@@ -1,9 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:netflix_clone/data/api/netflix_api.dart';
 import 'package:dio/dio.dart';
 
-final logger = Logger();
+final logger = Logger(
+  printer: PrettyPrinter(),
+);
 final netflixApiProvider = Provider<RestClient>((ref) {
-  return RestClient(Dio());
+  final dio = Dio();
+  dio.interceptors.add(
+    LogInterceptor(
+      request: false,
+      requestHeader: false,
+      requestBody: true,
+      responseHeader: false,
+      responseBody: true,
+      error: true,
+      logPrint: (o) {
+        if (kDebugMode) {
+          logger.log(Level.info, o.toString());
+        }
+      },
+    ),
+  );
+
+  return RestClient(dio);
 });
